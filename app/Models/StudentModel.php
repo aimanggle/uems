3d2879ff1328ec8cf0c4cbdeb2a3c317de411cbd
aimanggle <aimanggle21@gmail.com>
2022-10-26 22:28:23 +0800

    add new qeury

--- app/Models/StudentModel.php
+++ app/Models/StudentModel.php
@@ -9,4 +9,10 @@ class StudentModel extends Model
     protected $table            = 'student';
     protected $primaryKey       = 'sid';
     protected $allowedFields    = ['sid', 'studentid', 'fullname', 'programid', ];
+
+    public function detail($studentid)
+    {
+        $sql = "SELECT * FROM `student` WHERE studentid = '$studentid'";
+        return $this->db->query($sql)->getRow();
+    }
 }
