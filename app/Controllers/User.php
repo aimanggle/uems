983632dbf1d8f3a7a158c7856023a5eafec6ed0a
aimanggle <aimanggle21@gmail.com>
2022-10-27 21:50:58 +0800

    add new construct and update array in index public function

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,16 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\UserModel;
 use App\Controllers\BaseController;
 
 class User extends BaseController
 {
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
     public function index()
     {
         $data = [
             'title' => 'User',
-            // 'user' => $this->userModel->getUser(),
+            'user' => $this->userModel->findAll(),
         ];
+        // dd($data);
         return view('user/show', $data);
     }
 }
