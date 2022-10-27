ef4b0c83421bc62befcbcc391dfaaee0dca6041a
aimanggle <aimanggle21@gmail.com>
2022-10-27 21:50:23 +0800

    connect new model and orderby in sql query

--- app/Controllers/Listing.php
+++ app/Controllers/Listing.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\EventModel;
+use App\Models\StudentModel;
 use App\Controllers\BaseController;
 
 class Listing extends BaseController
@@ -10,13 +11,14 @@ class Listing extends BaseController
     public function __construct()
     {
         $this->eventModel = new EventModel();
+        $this->studentModel = new StudentModel();
     }
     
     public function index()
     {
         $data = [
             'title' => 'Listing | UEMS',
-            'event' => $this->eventModel->findAll()
+            'event' => $this->eventModel->orderBy('eventid', 'DESC')->findAll(),
         ];
         // d($data);
     
@@ -44,5 +46,49 @@ class Listing extends BaseController
             ];
             return view('listing/close', $data);
         }
+    }
+
+    public function register($eventid)
+    {
+        $data = [
+            'title' => 'Register | UEMS',
+            'event' => $this->eventModel->find($eventid)
+        ];
+        // d($data);
+    
+        return view('listing/register/step1', $data);
+    }
+
+    public function register2($eventid)
+    {
+        $studentid = $this->request->getVar('studentid');
+
+        if(empty($this->studentModel->detail($studentid)))
+        {   
+            return redirect()->back()->with('error', 'Student ID not found');
+        }
+        else
+        {
+            $data = [
+                'title' => 'Register | UEMS',
+                'event' => $this->eventModel->find($eventid),
+                'student' => $this->studentModel->detail($studentid)
+                
+            ];
+            // dd($data);
+            return view('listing/register/step2', $data);
+        }     
+    }
+
+    public function register3($eventid)
+    {
+        $studentid = $this->request->getVar('studentid');
+        $data = [
+            'title' => 'Register | UEMS',
+            'event' => $this->eventModel->find($eventid),
+            'student' => $this->studentModel->detail($studentid)
+        ];
+        // dd($data);
+        return view('listing/register/step3', $data);
     }
 }
