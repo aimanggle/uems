0df7f207571679812e23fbfeae7bf0b5be45a975
aimanggle <aimanggle21@gmail.com>
2022-11-13 17:05:28 +0800

    add new model  and fix query also add new method and solve find college prob

--- app/Controllers/Listing.php
+++ app/Controllers/Listing.php
@@ -3,7 +3,10 @@
 namespace App\Controllers;
 
 use App\Models\EventModel;
+use App\Models\CollegeModel;
+use App\Models\ProgramModel;
 use App\Models\StudentModel;
+use App\Models\RegistrantModel;
 use App\Models\EventCategoryModel;
 use App\Controllers\BaseController;
 
@@ -14,6 +17,9 @@ class Listing extends BaseController
         $this->eventModel = new EventModel();
         $this->studentModel = new StudentModel();
         $this->EventCategoryModel = new EventCategoryModel();
+        $this->RegistrantModel = new RegistrantModel();
+        $this->collegeModel = new CollegeModel();
+        $this->programModel = new ProgramModel();
     }
     
     public function index()
@@ -34,7 +40,7 @@ class Listing extends BaseController
         
         if($query)
         {
-            $event = $this->eventModel->like('eventname', $query)->orLike('eventtype', $query)->orLike('eventstatus', $query)->orLike('eventscorun',$query)->findAll();
+            $event = $this->eventModel->like('eventname', $query)->orLike('eventtype', $query)->orLike('eventstatus', $query)->orLike('eventscorun',$query)->orLike('eventcatname', $query)->findAll();
         }
         else
         {
@@ -95,8 +101,12 @@ class Listing extends BaseController
         {   
             $data=[
                 'title' => 'Register | UEMS',
-                'validation' => \Config\Services::validation()
+                'validation' => \Config\Services::validation(),
+                'college' => $this->collegeModel->findAll(),
+                'program' => $this->programModel->findAll(),
+                'studentid' => $studentid,
             ];
+            // d($data);
             // return redirect()->back()->with('error', 'Student ID not found');
             return view('listing/register/student', $data);
         }
@@ -121,7 +131,42 @@ class Listing extends BaseController
             'event' => $this->eventModel->find($eventid),
             'student' => $this->studentModel->detail($studentid)
         ];
-        d($data);
+        // d($data);
         return view('listing/register/step3', $data);
+    }
+
+    public function attemptRegister($eventid, $sid, $collegeid)
+    {
+        $data = [
+            'sid' => $sid,
+            'eventid' => $eventid,
+            'regdate' => date('Y-m-d H:i:s'),
+            'collegeid' => $collegeid,
+            
+        ];
+
+        $this->RegistrantModel->save($data);
+        return redirect()->to('/event/listing')->with('success', 'You have successfully registered for this event');
+    }
+
+    public function storestudent()
+    {
+        $data = [
+            'studentid' => $this->request->getVar('studentid'),
+            'fullname' => $this->request->getVar('fullname'),
+            'programid' => $this->request->getVar('program'),
+            'collegeid' => $this->request->getVar('college'),
+        ];
+        // dd($data);
+        $this->studentModel->save($data);
+
+    }
+
+    public function findcollege($collegeid)
+    {
+        
+        $program = $this->programModel->where('collegeid', $collegeid)->findAll();
+        return json_encode($program);
+        
     }
 }
