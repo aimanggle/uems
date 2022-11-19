31e7ae903aaf1e1f49d4b58b0035f36eb69cd29f
aimanggle <aimanggle21@gmail.com>
2022-11-19 09:12:12 +0800

    add documentation to code

--- app/Controllers/Listing.php
+++ app/Controllers/Listing.php
@@ -29,7 +29,8 @@ class Listing extends BaseController
     
     /**
      * ---------------------------------------------
-     * Public Methods
+     *  Methods for Event Listing
+     *  @param null
      * ---------------------------------------------
      */
     public function index()
@@ -40,10 +41,15 @@ class Listing extends BaseController
             'event' => $this->eventModel->OrderBy('eventid', 'DESC')->findAll(),
             'eventcat' => $this->EventCategoryModel->findAll()
         ];
-
         return view('listing/listing', $data);
     }
 
+    /**
+     * ---------------------------------------------
+     *  Methods for Event filter
+     *  @param varchar $query
+     * ---------------------------------------------
+     */
     public function filter($query)
     {
         
@@ -61,11 +67,15 @@ class Listing extends BaseController
             'event' => $event,
             'eventcat' => $this->EventCategoryModel->findAll()
         ];
-        // d($data);
         return view('listing/listing', $data);
     }
 
-
+    /**
+     * ---------------------------------------------
+     *  Methods for Event Detail
+     *  @param int $eventid
+     * ---------------------------------------------
+     */
     public function detail($eventid)
     {
         $eventcheck = $this->eventModel->where('eventid', $eventid)->first();
@@ -76,8 +86,6 @@ class Listing extends BaseController
                 'title' => 'Detail | UEMS',
                 'event' => $this->eventModel->find($eventid)
             ];
-            // dd($data);
-        
             return view('listing/detail', $data);
         }
         else
@@ -89,17 +97,27 @@ class Listing extends BaseController
         }
     }
 
+    /**
+     * ---------------------------------------------
+     *  Methods for Register Event
+     *  @param int $eventid
+     * ---------------------------------------------
+     */
     public function register($eventid)
     {
         $data = [
             'title' => 'Register | UEMS',
             'event' => $this->eventModel->find($eventid)
         ];
-        // d($data);
-    
         return view('listing/register/step1', $data);
     }
 
+    /**
+     * ---------------------------------------------
+     *  Methods for Register Event Step 2
+     *  @param int $eventid
+     * ---------------------------------------------
+     */
     public function register2($eventid)
     {
         $studentid = $this->request->getVar('studentid');
@@ -114,7 +132,6 @@ class Listing extends BaseController
                 'studentid' => $studentid,
                 'event' => $this->eventModel->find($eventid)
             ];
-
             return view('listing/register/student', $data);
         }
         else
@@ -125,11 +142,16 @@ class Listing extends BaseController
                 'student' => $this->studentModel->detail($studentid)
                 
             ];
-            // dd($data);
             return view('listing/register/step2', $data);
         }     
     }
 
+    /**
+     * ---------------------------------------------
+     *  Methods for Register Event Step 3
+     *  @param int $eventid
+     * ---------------------------------------------
+     */
     public function register3($eventid)
     {
         $studentid = $this->request->getVar('studentid');
@@ -138,10 +160,15 @@ class Listing extends BaseController
             'event' => $this->eventModel->find($eventid),
             'student' => $this->studentModel->detail($studentid)
         ];
-        // d($data);
         return view('listing/register/step3', $data);
     }
 
+    /**
+     * ---------------------------------------------
+     *  Methods for Attempt Register Event
+     *  @param int $eventid, $sid, $collegeid
+     * ---------------------------------------------
+     */
     public function attemptRegister($eventid, $sid, $collegeid)
     {
         $data = [
@@ -153,24 +180,25 @@ class Listing extends BaseController
 
         if($this->RegistrantModel->save($data))
         {
+            $regid = $this->RegistrantModel->insertID();
+            $regno = "#UEMS".date('y')."".$regid;
+            $this->RegistrantModel->update($regid, ['regno' => $regno]);
+
             $data1=[
                 'title' => 'Thank you | UEMS',
                 'event' => $this->eventModel->find($eventid),
+                'regno' => $regno
             ];
             return view('listing/register/thankyou', $data1);
-            // return redirect()->to('event/listing/register/thankyou');
         }
     }
 
-    public function thankyou()
-    {
-        $data1=[
-            'title' => 'Thank you | UEMS',
-        ];
-        return view('listing/register/thankyou', $data1);
-    }
-    
-
+    /**
+     * ---------------------------------------------
+     *  Methods for Store Student in Database
+     *  @param int $eventid
+     * ---------------------------------------------
+     */
     public function storestudent($eventid)
     {
         $data = [
@@ -180,15 +208,18 @@ class Listing extends BaseController
             'collegeid' => $this->request->getVar('college'),
         ];
         $this->studentModel->save($data);
-        return view('listing/register/thankyou', $data)->with('success', 'You have successfully register to the system');
-
+        return redirect()->to('/event/listing/register/'.$eventid)->with('success', 'You have successfully register to the system');
     }
 
+    /**
+     * ---------------------------------------------
+     *  Methods for find college
+     *  @param int $collegeid
+     * ---------------------------------------------
+     */
     public function findcollege($collegeid)
     {
-        
         $program = $this->programModel->where('collegeid', $collegeid)->findAll();
         return json_encode($program);
-        
     }
 }
