99ae2b66692c3f22a7e106f19254c01bbc97ae3f
aimanggle <aimanggle21@gmail.com>
2022-12-30 10:08:44 +0800

    add announcement controller

--- app/Controllers/Announcement.php
+++ app/Controllers/Announcement.php
@@ -2,12 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\AnnouncementModel;
 use App\Controllers\BaseController;
 
 class Announcement extends BaseController
 {
+    
+    public function __construct()
+    {
+        //load model
+        $this->announcementModel = new AnnouncementModel();
+    }
+
     public function index()
     {
-        //
+        $data = [
+            'title' => 'Announcement | UEMS',
+            'announce' => $this->announcementModel->findAll(),
+        ];
+        return view('announcement/show', $data);
     }
 }
